d51b4f03c76663fc3f596c7d95ec6479dc4e65db
ryannguyen275 <ryannguyen275@gmail.com>
2023-05-15 14:38:28 -0500

    attempt 2 tuning

---
+++ attempt_2/02_knn_tuned.R
@@ -0,0 +1,37 @@
+# K-Nearest Neighbors Tuning
+
+##### LOAD PACKAGES/DATA ##############################################
+
+library(tidymodels)
+library(tidyverse)
+
+library(doMC)
+registerDoMC(cores = 4)
+
+load("attempt_2/setups/setup_1.rda")
+
+##### DEFINE ENGINES/WORKFLOWS #########################################
+knn_model <- nearest_neighbor(mode = "regression",
+                              neighbors = tune()) %>% 
+  set_engine("kknn")
+
+knn_param <- extract_parameter_set_dials(knn_model)
+
+knn_grid <- grid_regular(knn_param, levels = 5)
+
+knn_workflow <- workflow() %>% 
+  add_model(knn_model) %>% 
+  add_recipe(recipe1)
+
+##### TUNE GRID ########################################################
+knn_tuned <- tune_grid(knn_workflow,
+                       resamples = folds,
+                       verbose = TRUE,
+                       grid = knn_grid,
+                       control = control_grid(save_pred = TRUE, 
+                                              save_workflow = FALSE,
+                                              verbose = TRUE,
+                                              parallel_over = "everything"))
+
+save(knn_tuned, file = "attempt_2/results/knn_tuned.rda")
+
---
+++ attempt_2/02_rf_tuned.R
@@ -0,0 +1,41 @@
+
+##### LOAD PACKAGES/DATA ##############################################
+
+library(tidymodels)
+library(tidyverse)
+library(tictoc)
+
+library(doMC)
+registerDoMC(cores = 4)
+
+tidymodels_prefer ()
+
+load("attempt_2/setups/setup_1.rda")
+
+##### DEFINE ENGINES/WORKFLOWS #########################################
+rf_model <- rand_forest(min_n = tune(),
+                        mtry = tune()) %>% 
+  set_engine("ranger", importance = "impurity") %>% 
+  set_mode("regression")
+
+rf_param <- extract_parameter_set_dials(rf_model) %>% 
+  update(mtry = mtry(range = c(1,10)))
+
+rf_grid <- grid_regular(rf_param, levels = 5)
+
+rf_workflow <- workflow() %>% 
+  add_model(rf_model) %>% 
+  add_recipe(recipe1)
+
+##### TUNE GRID ########################################################
+rf_tuned <- tune_grid(rf_workflow,
+                      resamples = folds,
+                      grid = rf_grid,
+                      verbose = TRUE,
+                      control = control_grid(save_pred = TRUE, 
+                                             save_workflow = TRUE,
+                                             verbose = TRUE,
+                                             parallel_over = "everything"))
+
+
+save(rf_tuned, file = "attempt_2/results/rf_tuned.rda")
