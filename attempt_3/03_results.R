a5fc33b56ebe2ec325d13d4f712b972ad380c11a
ryannguyen275 <ryannguyen275@gmail.com>
2023-05-16 00:47:55 -0500

    attempt 3 results, submission 22

--- attempt_3/03_results.R
+++ attempt_3/03_results.R
@@ -1,10 +1,9 @@
 # Model Results
 library(tidyverse)
 library(tidymodels)
+library(stacks)
 
 tidymodels_prefer()
-
-registerDoMC(cores = 4)
 
 ##### LOAD PACKAGES/DATA ##############################################
 load("attempt_3/setups/setup_1.rda")
@@ -15,21 +14,26 @@ load("attempt_3/results/svm_radial_tuned.rda")
 load("attempt_3/results/svm_poly_tuned.rda")
 
 data_stacks <- stacks() %>%
-  add_candidates(rf_tuned) %>% # 15 models
-  add_candidates(svm_radial_tuned) %>% # 1 model
+  add_candidates(rf_tuned) %>%
+  add_candidates(svm_radial_tuned) %>% 
   add_candidates(svm_poly_tuned)
 
 as_tibble(data_stacks)
+
+data_model <- data_stacks %>% 
+  blend_predictions(penalty = 10^(-6:-1))
+
+data_model <- data_model %>% 
+  fit_members()
+
+save(data_model, file = "attempt_3/results/data_model.rda")
  
 ##### FINAL FIT ######################################################
-svm_poly_workflow <- svm_poly_workflow %>% 
-  finalize_workflow(select_best(svm_poly_tuned, metric = "rmse"))
 
-final_fit <- fit(svm_poly_workflow, train_data)
-
-predictions <- predict(final_fit, test) %>% 
-  bind_cols(test %>% select(id)) %>% 
+predictions <- test %>%
+  bind_cols(predict(data_model, .)) %>% 
+  select(id, .pred) %>% 
   rename(y = .pred)
 
-write_csv(predictions, file = "submissions/21_submission.csv")
+write_csv(predictions, file = "submissions/22_submission.csv")
 
